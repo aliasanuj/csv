92dcf1e7a0a352438f5cf25b8b56482f4865ad20
kanuj112 <k.kumaranuj112@gmail.com>
2023-04-22 01:10:56 +0530

    some csv operations

--- csvWrite.py
+++ csvWrite.py
@@ -1,4 +1,4 @@
-#to create a new file using python
+#to create a new csv file using python
 # working
 # import csv
 # # Define the headers
@@ -35,17 +35,52 @@
 #     objectFile.writerow(header)
 #     objectFile.writerows(data)
 
+
+
 # code for creating multiple sheets in xlsx using pyython
 # working
-import pandas as pd
-import re
-df1 = pd.DataFrame({'Data': ['arter', 'berger', 'cretre', 'd45yt45']})
-print(type(df1))
-df2 = pd.DataFrame({'Data': [1, 25, 35454, 4545]})
-df3 = pd.DataFrame({'Data': [1.15454, 1.25, 1.34353, 1.46545]})
-writer = pd.ExcelWriter(r'C:\Git\csv\filename.xlsx', engine='xlsxwriter')
-df1.to_excel(writer, sheet_name='Sheeta')
-df2.to_excel(writer, sheet_name='Sheetb')
-df3.to_excel(writer, sheet_name='Sheetc')
-writer._save()
+# import pandas as pd
+# import re
+# df1 = pd.DataFrame({'Data': ['arter', 'berger', 'cretre', 'd45yt45']})
+# print(type(df1))
+# df2 = pd.DataFrame({'Data': [1, 25, 35454, 4545]})
+# df3 = pd.DataFrame({'Data': [1.15454, 1.25, 1.34353, 1.46545]})
+# writer = pd.ExcelWriter(r'C:\Git\csv\filename.xlsx', engine='xlsxwriter')
+# df1.to_excel(writer, sheet_name='Sheeta')
+# df2.to_excel(writer, sheet_name='Sheetb')
+# df3.to_excel(writer, sheet_name='Sheetc')
+# writer._save()
+#
+
+
+
+# clean code for creating multiple sheets in xlsx using pyython
+# import the python pandas package
+# import pandas as pd
+# import re
+# # create  data_frame1 by creating a dictionary
+# # in which values are stored as list
+# data_frame1 = pd.DataFrame({'Fruits': ['Appple', 'Banana', 'Mango',
+#                                        'Dragon Fruit', 'Musk melon', 'grapes'],
+#                             'Sales in kg': [20, 30, 15, 10, 50, 40]})
+# # create  data_frame2 by creating a dictionary
+# # in which values are stored as list
+# data_frame2 = pd.DataFrame({'Vegetables': ['tomato', 'Onion', 'ladies finger',
+#                                            'beans', 'bedroot', 'carrot'],
+#                             'Sales in kg': [200, 310, 115, 110, 55, 45]})
+# # create  data_frame3 by creating a dictionary
+# # in which values are stored as list
+# data_frame3 = pd.DataFrame({'Baked Items': ['Cakes', 'biscuits', 'muffins',
+#                                             'Rusk', 'puffs', 'cupcakes'],
+#                             'Sales in kg': [120, 130, 159, 310, 150, 140]})
+# print(data_frame1)
+# print(data_frame2)
+# print(data_frame3)
+# # create a excel writer object
+# with pd.ExcelWriter(r"filename01.xlsx") as writer:
+#     # use to_excel function and specify the sheet_name and index
+#     # to store the dataframe in specified sheet
+#     data_frame1.to_excel(writer, sheet_name="Fruits", index=False)
+#     data_frame2.to_excel(writer, sheet_name="Vegetables", index=False)
+#     data_frame3.to_excel(writer, sheet_name="Baked Items", index=False)
 
